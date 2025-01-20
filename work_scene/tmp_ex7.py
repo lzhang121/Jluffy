4c12a02f035c9843eccf7769d2c6d7eb396dcdff
张雷 <zhanglei@bonree.com>
2025-01-20 22:21:06 +0900

    更新

--- work_scene/tmp_ex7.py
+++ work_scene/tmp_ex7.py
@@ -13,6 +13,10 @@ def main():
     path_test_2 = "It is\\n ok"
     print(path_test_1)
     print(path_test_2)
+    test_list = list(range(10))
+    print(test_list)
+    print(test_list[0:0])
+    print(type(test_list[0:1]))
 
 
 if __name__ == '__main__':
